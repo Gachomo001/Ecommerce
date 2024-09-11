bdb96dec3228711ab928376a52eed5db7aec2816
Gachomo001 <allangachomo37@gmail.com>
2024-09-11 18:46:33 +0300

    footflair project

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\Admin;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Admin::create([
+            'first_name'=>'Admin',
+            'middle_name'=>'John',
+            'last_name'=>'Doe',
+            'username'=>'admin',
+            'email'=>'admin@email.com',
+            'password'=>Hash::make('12345')
+        ]);
+    }
+}
